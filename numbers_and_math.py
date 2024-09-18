416f6057c774a1ac5e006b8e443631309df60adc
TheN3RVOUSGuy-FBLIVE <vincent.huang26@ycdsbk12.ca>
2024-09-18 17:47:57 -0400

    Add files via upload

---
+++ numbers_and_math.py
@@ -0,0 +1,26 @@
+print("There are 11 girls, so that means..")
+# subtracts 11 from 33
+print(f"there are {33 - 11} boys.")
+print()
+print("I have a class of 33 students.")
+# divides 11 by 33
+print(f"That means {round(11 / 33, 2)} % are girls...")
+# subtracts 11 from 33 and divides by 33
+print(f"and {round((33 - 11) / 33, 2)} % are boys.")
+print()
+print("If we made groups of six...")
+# floor divides 33 by 6 (rounds final value down)
+print(f"There would be {33 // 6} groups of six.")
+# divides and returns remainder
+print(f"And then a smaller group of {33 % 6} people.")
+# prints "-" 30 times
+print("-" * 30)
+print("If we had 17 apples and 3 people...")
+# divides and round down
+print(f"Each person would get {17 // 3} whole apples.")
+# returns remainder
+print("There would be " + str(17 % 3) + " apples remaining.")
+print() 
+print("If we charged each person $2 each for their 5 apples..")
+# same as line 9 (multiplication)
+print(f"they would each pay ${2 * 5}.")
